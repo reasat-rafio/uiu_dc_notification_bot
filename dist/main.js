f9e7e38ad6a2a7b0cbc9dc55d8b2efc85f1052ae
reasat-rafio <rafioar96@gmail.com>
2021-09-19 18:58:47 +0600

    :rocket: deploy

--- dist/main.js
+++ dist/main.js
@@ -3,8 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app/app.module");
 async function bootstrap() {
+    const PORT = process.env.PORT || 3000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
